7553c802890a860f25823630a8f45c35aa5d744a
Ben Morales <b-morales@kellogg.northwestern.edu>
2013-10-31 00:12:16 -0500

    Install Rails files

---
+++ final_project_v4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinalProjectV4::Application.config.secret_key_base = 'be6cb3e60a16f355b70ed1d5714f9ae8dcf798ad6d07ce74022de31274029b2768d8028dbf249b20065f9911adbdd93ad720b1b5afc4a2cc12822d28945f7edd'
---
+++ final_project_v4/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FinalProjectV4::Application.config.session_store :cookie_store, key: '_final_project_v4_session'
